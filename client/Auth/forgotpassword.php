0e3820e786f559ea5dbd56532d7836da0b4fa1a5
marlieer <marlie_97@hotmail.ca>
2020-03-10 09:56:51 -0700

    adjusting forms for consistency

--- client/Auth/forgotpassword.php
+++ client/Auth/forgotpassword.php
@@ -1,17 +1,21 @@
 <?php include '../top.php'; ?>
-<form name="forgotpassform" method="post" action="../server/auth.php" onsubmit="return forgotPassword()">
-	<fieldset>
-	<legend>Forgot Password</legend>
-		<p>
-			Please enter your email. An email will  be sent to reset your password. 
-		</p>
-		<p>
-			<label>Email</label>
-			<input class="form-control" type="text" name="email"/>
-        </p>
-        <br/>
-		<button type="submit" class="btn my-btn" name="forgot-submit">Submit</button>
-		<a href="login.php">Go Back</a>
-	</fieldset>
-</form>
-<?php include '../top.php'; ?>
+    <main>
+        <div class="container">
+            <form name="forgotpassform" method="post" action="../server/auth.php" onsubmit="return forgotPassword()">
+                <fieldset>
+                    <legend>Forgot Password</legend>
+                    <p>
+                        Please enter your email. An email will be sent to reset your password.
+                    </p>
+                    <p>
+                        <label>Email</label>
+                        <input class="input form-control" type="text" name="email"/>
+                    </p>
+                    <br/>
+                    <button type="submit" class="btn my-btn" name="forgot-submit">Submit</button>
+                    <a href="login.php">Go Back</a>
+                </fieldset>
+            </form>
+        </div>
+    </main>
+<?php include '../bottom.php'; ?>
