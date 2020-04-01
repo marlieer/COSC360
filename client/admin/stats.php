44aac8be68679afef14ad1b9bb26cdf790787fd1
Khoi <khoi16ngo@gmail.com>
2020-04-01 13:12:32 -0600

    change admin stats

--- client/admin/stats.php
+++ client/admin/stats.php
@@ -31,7 +31,7 @@ th,td{
             <td rowspan="3"><div id="bar_chart"></div></td>
         </tr>
         <tr>
-            <th class="th-1">Top 5 Searched Topics</th>
+            <th class="th-1">Top 5 Viewd Categories</th>
             <td>
                 <ol>
                 <?php $data = get_top_5();
